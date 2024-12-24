47e7be5a02a260e0b0e8bcc57cbe2fb90bdb1521
JohnyVasamsetti <vasamsetti.johny@gmail.com>
2024-12-24 20:26:29 +0530

    [Johny] Keep track of claimed users to not process the claim again.

--- src/MerkleAirdrop.sol
+++ src/MerkleAirdrop.sol
@@ -9,9 +9,11 @@ contract MerkleAirdrop {
     using SafeERC20 for IERC20;
 
     error MerkleAirdrop__InvalidProof();
+    error MerkleAirdrop__HasAlreadyClaimed(address account);
 
     bytes32 private immutable i_merkleRoot;
     IERC20 private immutable i_airdropToken;
+    mapping(address account => bool isClaimed)  private s_isClaimed;
 
     event Claim(address indexed account, uint256 amount);
 
@@ -21,11 +23,17 @@ contract MerkleAirdrop {
     }
 
     function claim(address account, uint256 amount, bytes32[] calldata merkleProof) external {
+        if(s_isClaimed[account] == true){
+            revert MerkleAirdrop__HasAlreadyClaimed(account);
+        }
         bytes32 leaf = keccak256(bytes.concat(keccak256(abi.encode(account ,amount))));
         if (MerkleProof.verify(merkleProof,i_merkleRoot,leaf)) {
             revert MerkleAirdrop__InvalidProof();
         }
+
+        s_isClaimed[account] = true;
         emit Claim(account,amount);
+
         i_airdropToken.safeTransfer(account,amount);
     }
 }

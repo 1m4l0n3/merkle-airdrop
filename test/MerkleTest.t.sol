82839362bd6ab1a507386bedcf1471dc270b49ab
JohnyVasamsetti <vasamsetti.johny@gmail.com>
2024-12-24 23:49:05 +0530

    [Johny] Add test script for Merkle & BagelToken

---
+++ test/MerkleTest.t.sol
@@ -0,0 +1,28 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.24;
+
+import {Test} from "../lib/forge-std/src/Test.sol";
+import {MerkleAirdrop} from "../src/MerkleAirdrop.sol";
+import {BagelToken} from "../src/BagelToken.sol";
+import {MerkleDeploy} from "../script/MerkleDeploy.s.sol";
+
+contract MerkleTest is Test {
+    bytes32 private root;
+    MerkleAirdrop private merkleAirdrop;
+    BagelToken private bagelToken;
+    MerkleDeploy private deployer;
+
+    function setUp () external {
+        deployer = new MerkleDeploy();
+        (bagelToken,merkleAirdrop,root) = deployer.run();
+    }
+
+    function testTokenInformation() external view {
+        string memory expectedName = "Bagel Token";
+        string memory expectedSymbol = "BT";
+
+        assertEq(expectedName,bagelToken.name());
+        assertEq(expectedSymbol,bagelToken.symbol());
+    }
+}
